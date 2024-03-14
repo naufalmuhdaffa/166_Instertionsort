cdabf8e53ab51cbc52add7e3dc0c616772fc65bb
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-14 10:15:21 +0700

    Add project files.

---
+++ Instertion Sort/Instertion Sort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int arr[20];                // Membuat Array dengan panjang data 20
+int n;                      // Membuat Variabel inputan n
+
+int main()
+{
+    
+}
