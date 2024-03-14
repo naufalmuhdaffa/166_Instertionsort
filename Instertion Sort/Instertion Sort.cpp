efa2ea83e7ed06d7eab7f1afd215a358750be30d
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-14 11:21:51 +0700

    Menambahkan prosedur display

--- Instertion Sort/Instertion Sort.cpp
+++ Instertion Sort/Instertion Sort.cpp
@@ -56,6 +56,18 @@ void insertionsort() {                              // Procedure Insertionsort
     }
 }
 
+void display() {                                            // Procedure Display
+    cout << endl;
+    cout << "\n===============================" << endl;    // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl;    // Output ke layar
+    cout << "=================================" << endl;    // Output ke layar
+
+    for (int j = 0; j < n; j++) {                           // Looping dengan j dimulai dari 0 hinggan n-1
+        cout << arr[j] << endl;                             // Output ke layar
+    }
+    cout << endl;                                           // Output baris kosong
+}
+
 int main()
 {
     
