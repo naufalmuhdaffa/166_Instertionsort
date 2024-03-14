4b97b1620bb5383da8cd4ac3c92f12bd77592bcf
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-14 10:28:10 +0700

    Menambahkan prosedur input: - Membuat inputan jumlah elemen Array - Memanggil Variabel inputan n - Membuat kondisi n tidak lebih dari 20 - Menampilkan output pesan jika data lebih dari 20

--- Instertion Sort/Instertion Sort.cpp
+++ Instertion Sort/Instertion Sort.cpp
@@ -3,6 +3,22 @@ using namespace std;
 int arr[20];                // Membuat Array dengan panjang data 20
 int n;                      // Membuat Variabel inputan n
 
+void input() {                                                              // Procedure Input
+    while (true)
+    {
+        cout << "Masukkan jumlah data array : ";                            // Membuat Inputan jumlah elemen Array
+        cin >> n;                                                           // Memanggil Variabel inputan n
+
+        if (n <= 20) {                                                      // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";     // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+}
+
 int main()
 {
     
