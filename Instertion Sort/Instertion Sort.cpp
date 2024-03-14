0081a6227324f64eba7d8fd9862e0899ea78705f
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-03-14 11:12:27 +0700

    Menambahkan output ke layar dan looping.

--- Instertion Sort/Instertion Sort.cpp
+++ Instertion Sort/Instertion Sort.cpp
@@ -48,6 +48,11 @@ void insertionsort() {                              // Procedure Insertionsort
         }
 
         arr[j + 1] = temp;                          // 5. Simpan nilai temp ke dalam arr[j+1]
+
+        cout << "\nPass " << i << ": ";         // Output ke layar
+        for (int k = 0; k < n; k++) {           // Looping nilai k dimulai dari 0 hingga n-1
+            cout << arr[k] << " ";              // Output ke layar
+        }
     }
 }
 
